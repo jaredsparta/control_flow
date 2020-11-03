f8e268cd50609cb3d4420ea7769faaa0050e1141
jared <jtlss14@gmail.com>
2020-11-03 15:48:40 +0000

    task 1

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Control Flow
 - if, elif, else etc.
+- for loops and while loops
---
+++ task_1.py
@@ -0,0 +1,12 @@
+age = input("How old are you? ")
+
+if age < 12:
+    print("You can only watch U films and PG films if accompanied by parents")
+elif 12 <= age < 15:
+    print("You can watch U, PG AND 12 films")
+elif 15 <= age < 18:
+    print("You can watch U, PG, 12 AND 15 films")
+elif age >= 18:
+    print("You can watch any film available")
+else:
+    print("ERROR")       
